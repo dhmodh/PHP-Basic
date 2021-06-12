4340a87c6bd83afcdec8c66f0403ec63b0a45b8f
dmodh <18itubn116@ddu.ac.in>
2021-06-12 09:54:03 +0530

    Basic of OOP

---
+++ OOP/Constructor&Destructors/index.php
@@ -0,0 +1,19 @@
+<?php
+	include 'includes/person.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<?php
+		$person1 = new Person("DMX", "Black", 20);
+		echo $person1 = getName();
+	?>
+
+</body>
+</html>
